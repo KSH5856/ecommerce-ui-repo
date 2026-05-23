49d98d3a8f5f015543ca40e421033b57db2d4d9c
Kashish <kashishgupta5856@gmail.com>
2026-05-23 16:35:42 +0530

    feat: added route in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function HomePage() {
+  const route = useRouter();
+
+  useEffect(() => {
+    route.push("/ecommerce/home");
+  }, []);
   return <></>;
 }
